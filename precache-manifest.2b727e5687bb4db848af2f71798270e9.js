c5827ea1500e77a3e206068462ad2e97aebfd092
Syl0 <nutechian@gmail.com>
2021-02-13 23:55:30 +0000

    Updates

---
+++ precache-manifest.2b727e5687bb4db848af2f71798270e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738538302cf49ce881a640447f2ebebb",
+    "url": "/BotSpace/index.html"
+  },
+  {
+    "revision": "104d4af027bc140b439b",
+    "url": "/BotSpace/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "2613d6f41d7b8b5b5a89",
+    "url": "/BotSpace/static/css/main.ed8cdd01.chunk.css"
+  },
+  {
+    "revision": "104d4af027bc140b439b",
+    "url": "/BotSpace/static/js/2.524e8e10.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/BotSpace/static/js/2.524e8e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1004ddc5c497d01dfc9",
+    "url": "/BotSpace/static/js/3.960c15a3.chunk.js"
+  },
+  {
+    "revision": "2613d6f41d7b8b5b5a89",
+    "url": "/BotSpace/static/js/main.dacf8105.chunk.js"
+  },
+  {
+    "revision": "735aaad1028c17f62c6f",
+    "url": "/BotSpace/static/js/runtime-main.41b71a6c.js"
+  },
+  {
+    "revision": "670908712b3f845cde23a131f0185903",
+    "url": "/BotSpace/static/media/Futuristic Armour.67090871.woff"
+  }
+]);
